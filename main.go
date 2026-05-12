91d98438d8d1fa998a4f500dea7b7b37e5ac1364
rhit-ciminodj <ciminodj@rose-hulman.edu>
2026-05-12 14:50:31 -0400

    Beginning Connection

--- main.go
+++ main.go
@@ -1,7 +1,47 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"net"
+	"time"
+)
 
 func main() {
-    fmt.Println("Hello, World!")
+	connect_port("irc.libera.chat", "6667")
+}
+
+func connect_port(host string, port string) {
+	timeout := time.Second
+	conn, err := net.DialTimeout("tcp", net.JoinHostPort(host, port), timeout)
+	if err != nil {
+		fmt.Println("Connecting Error: ", err)
+	}
+	if conn != nil {
+		defer conn.Close()
+		fmt.Println("Opened", net.JoinHostPort(host, port))
+		status, err := bufio.NewReader(conn).ReadString('\n')
+		fmt.Println(status)
+		fmt.Println(err)
+		connect_user(conn)
+	}
+}
+
+func connect_user(conn net.Conn) {
+
+	nickCommand := "NICK testUser\r\n"
+	nickByte := []byte(nickCommand)
+
+	conn.Write(nickByte)
+
+	userCommand := "USER testUser * * :Hunter Two\r\n"
+	userByte := []byte(userCommand)
+
+	conn.Write(userByte)
+
+	for {
+		status, err := bufio.NewReader(conn).ReadString('\n')
+		fmt.Println(status)
+		fmt.Println(err)
+	}
 }
